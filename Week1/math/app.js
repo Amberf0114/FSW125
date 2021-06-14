9083dc7499454cb7e907a90899db83ba65f647ef
unknown <amber.freeman@bryanuniversity.edu>
2021-06-13 20:22:55 -0400

    week1

---
+++ Week1/math/app.js
@@ -0,0 +1,9 @@
+const mathObj = require('./math')
+
+console.log(mathObj.sum(1,2))
+console.log(mathObj.difference(1,2))
+console.log(mathObj.product(1,2))
+console.log(mathObj.dividend(1,2))
+
+// how to see in my VSC console?
+// help syncing to github
---
+++ Week1/math/math.js
@@ -0,0 +1,23 @@
+//Ctrl v and c just stopped working
+const sum = (a, b) => {
+    return(a + b)
+}
+
+const difference = (a, b) => {
+    return(a - b)
+}
+
+const product = (a, b) => {
+    return(a * b)
+}
+
+const dividend = (a, b) => {
+    return(a / b)
+}
+
+module.exports = {
+    sum,
+    product,
+    difference,
+    dividend
+}
