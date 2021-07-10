ee9811c6c2f31d18554b88c3e8fcc557344bfd75
unknown <amber.freeman@bryanuniversity.edu>
2021-07-10 14:45:34 -0400

    commit

--- Week3/ExpressRouter/server.js
+++ Week3/ExpressRouter/server.js
@@ -2,13 +2,11 @@ const express = require('express')
 const app = express()
 
 
-const bountyRouter = express.Router()
-
 const PORT = 3000
 
 app.use(express.json())
 
-app.use('/bounties', bountyRouter)
+app.use('/bounties', require('./bountyRouter'))
 
 app.listen(PORT, () => {
     console.log(`App started on port: ${PORT}`)
