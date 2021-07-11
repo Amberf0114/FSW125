1cd2e16c7bac2b44e4cefad4f8c2bef64ec6f97a
unknown <amber.freeman@bryanuniversity.edu>
2021-07-11 12:14:39 -0400

    commit

---
+++ Week4/Project2/ToDoExpress.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const { v4 : uuidv4 } = require('uuid')
+
+app.use(express.json())
+
+const toDoRouter = express.Router()
+
+let toDoItems = [{
+    name: "Living Area", 
+    description : "sweep, mop, dust",
+    imageUrl : "",
+    completed : false,
+    _id : uuidv4()
+}]
+
+toDoRouter.route('/')
+    .get((req,res) => {
+        res.send(toDoItems)
+    })
+
+    .post((req,res) => {
+        const newItem = req.body
+        newItem._id = uuidv4()
+        toDoItems.push(newItem)
+
+        res.send(`Succsessfully added ${newItem} to your To-Do list!`)
+    })
+
+    //empty body when trying to get one
+toDoRouter.route('/:todoId')
+    .get((req,res) => {
+        const toDoId = req.params.toDoId
+        const foundItem = toDoItems.find(toDoItems => toDoItems._id === toDoId)
+        res.send(foundItem)
+    })
+
+module.exports = toDoRouter
